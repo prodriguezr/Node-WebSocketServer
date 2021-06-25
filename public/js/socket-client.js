260b72326a4b7b99a6dc9eb0b3b7f59d32e66e40
Pablo Rodríguez <prodrigu75@gmail.com>
2021-06-25 13:43:46 -0400

    Basic state change detection

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,9 +1,19 @@
+// HTML Refs
+const lblOnline  = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+
 const socket = io();
 
 socket.on('connect', () => {
     console.log('Connected');
+
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
 });
 
 socket.on('disconnect', () => {
     console.log('Disconnected');
+    
+    lblOnline.style.display = 'none';
+    lblOffline.style.display = '';
 });
