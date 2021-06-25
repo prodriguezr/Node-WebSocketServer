b95f160bde17dad35458e9abd71659269091054e
Pablo Rodríguez <prodrigu75@gmail.com>
2021-06-25 13:04:06 -0400

    Added socket.io

--- src/server/server.js
+++ src/server/server.js
@@ -6,8 +6,10 @@ const morgan = require('morgan');
 
 class Server {
     constructor() {
-        this.app = express();
-        this.port = process.env.PORT;
+        this.app    = express();
+        this.port   = process.env.PORT;
+        this.server = require('http').createServer(this.app);
+        this.io     = require('socket.io')(this.server);
 
         this.middlewares();
     }
@@ -20,7 +22,7 @@ class Server {
     }
 
     listen() {
-        this.app.listen(this.port, () => {
+        this.server.listen(this.port, () => {
             console.log(`Server listening at http://localhost:${this.port}`)
         });
     }
